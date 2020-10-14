db51c479eedc5fa88d24178325e42bef1862758c
Juan Alvarez <jualvarezlo@unal.edu.co>
2020-10-13 19:45:38 -0500

    Implementacion de todos los login

---
+++ app/src/main/java/com/proyecto/daily_food/AuthActivity.kt
@@ -0,0 +1,190 @@
+package com.proyecto.daily_food
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_auth.*
+import java.security.Provider
+
+class AuthActivity : AppCompatActivity() {
+
+    private val GOOGLE_SIGN_IN = 100
+
+    private val callBackManager = CallbackManager.Factory.create()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        //Setup
+        setup()
+        session()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        authLayout.visibility = View.VISIBLE
+    }
+
+    private fun session(){
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+
+        if(email != null && provider != null){
+            authLayout.visibility = View.INVISIBLE
+            showHome(email, ProviderType.valueOf(provider))
+        }
+    }
+
+    private fun setup(){
+
+        title = "Autenticacion"
+
+        facebookButton.setOnClickListener {
+
+            LoginManager.getInstance().logInWithReadPermissions(this, listOf("email"))
+
+            LoginManager.getInstance().registerCallback(callBackManager,
+                object : FacebookCallback<LoginResult>{
+                    override fun onSuccess(result: LoginResult?) {
+                        result?.let {
+
+                            val token = it.accessToken
+
+                            val credential = FacebookAuthProvider.getCredential(token.token)
+
+                            FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener{
+                                if (it.isSuccessful){
+                                    showHome(it.result?.user?.email ?: "", ProviderType.FACEBOOK)
+                                }else{
+                                    showAlert()
+                                }
+                            }
+                        }
+                    }
+
+                    override fun onCancel() {
+                        setup()
+                    }
+
+                    override fun onError(error: FacebookException?) {
+                        showAlert()
+                    }
+                })
+        }
+
+        googleButton.setOnClickListener{
+
+            //Configurar
+
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+
+        }
+
+        registerButton.setOnClickListener{
+            if (emailEditText.text.isNotEmpty() && paswordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString()
+                    , paswordEditText.text.toString()).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    }else{
+                        showAlert()
+                    }
+                }
+
+            }
+        }
+
+        logInButton.setOnClickListener{
+            if (emailEditText.text.isNotEmpty() && paswordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEditText.text.toString()
+                    , paswordEditText.text.toString()).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    }else{
+                        showAlert()
+                    }
+                }
+
+            }
+        }
+    }
+
+    private fun showAlert(){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType){
+
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        callBackManager.onActivityResult(requestCode, resultCode, data)
+
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == GOOGLE_SIGN_IN){
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if(account != null) {
+
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener{
+                        if (it.isSuccessful){
+                            showHome(account.email?:"", ProviderType.GOOGLE)
+                        }else{
+                            showAlert()
+                        }
+                    }
+                }
+
+            }catch (e: ApiException){
+                showAlert()
+            }
+        }
+    }
+}
